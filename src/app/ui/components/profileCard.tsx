d829176fd159d1a7507d4cfcea741f9dbedd422f
Duck Method <129229008+Duck-Method@users.noreply.github.com>
2025-01-27 21:30:27 -0700

    created the profilecard component

---
+++ src/app/ui/components/profileCard.tsx
@@ -0,0 +1,10 @@
+// user's profile card component
+
+export default function ProfileCard() {
+    return (
+        <div>
+            <h1>Users Profile</h1>
+            <p>Users page content</p>
+        </div>
+    );
+}
