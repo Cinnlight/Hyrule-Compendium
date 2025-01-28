0e65419a3d71245d99c5c53a8e9e3f6b51c1c531
CinnlShiro <cinnlight@gmail.com>
2025-01-27 21:08:15 -0700

    Adds several additional fields

--- src/server/src/models/reactions.ts
+++ src/server/src/models/reactions.ts
@@ -4,14 +4,20 @@ import { Sequelize, Model, DataTypes, Optional } from "sequelize";
 // Define attributes for the Reaction
 interface ReactionAttributes {
     id: number;
+    comment_id: number;
+    reaction_id: number;
     emoji_url: string;
+    count?: number;
 }
 
 interface ReactionCreationAttributes extends Optional<ReactionAttributes, "id"> {}
 
 export class Reactions extends Model<ReactionAttributes, ReactionCreationAttributes> implements ReactionAttributes {
     public id!: number;
+    public comment_id!: number;
+    public reaction_id!: number;
     public emoji_url!: string;
+    public count?: number;
 }
 
 export function ReactionFactory(sequelize: Sequelize): typeof Reactions {
@@ -22,10 +28,22 @@ export function ReactionFactory(sequelize: Sequelize): typeof Reactions {
                 primaryKey: true,
                 autoIncrement: true,
             },
+            comment_id: {
+                type: DataTypes.INTEGER,
+                allowNull: false,
+            },
+            reaction_id: {
+                type: DataTypes.INTEGER,
+                allowNull: false,
+            },
             emoji_url: {
                 type: DataTypes.STRING,
                 allowNull: false,
             },
+            count: {
+                type: DataTypes.INTEGER,
+                defaultValue: 0,
+            }
         },
         {
             modelName: "Reactions",
