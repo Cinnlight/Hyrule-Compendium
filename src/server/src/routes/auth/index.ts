fa8e2270de1a4420a501d38e41ced92c63e18817
MagicInUse <jacob.norr@live.com>
2025-01-30 19:53:46 -0700

    registration almost working, looks like some database things to clean up

--- src/server/src/routes/auth/index.ts
+++ src/server/src/routes/auth/index.ts
@@ -8,10 +8,10 @@ const authController = new AuthController();
 const emailRouter = express.Router();
 const emailController = new EmailController();
 
-authRouter.post('/auth/login', authController.login);
-authRouter.post('/auth/logout', authController.logout);
-authRouter.post('/auth/register', authController.register);
-authRouter.post('/auth/resend-verification', authController.resendVerification);
+authRouter.post('/login', authController.login);
+authRouter.post('/logout', authController.logout);
+authRouter.post('/register', authController.register);
+authRouter.post('/resend-verification', authController.resendVerification);
 
 emailRouter.get('/email/verify', emailController.emailVerificationCallback);
 emailRouter.post('/email/verification', emailController.emailVerificationRequest);
