bb8eb6e050b1befe0ee42b35f1c74197216a6da7
dauntss <kat.krahulec@gmail.com>
2025-01-30 21:57:55 -0700

    finished basic structure of profile page

--- src/app/ui/components/profileCard.tsx
+++ src/app/ui/components/profileCard.tsx
@@ -2,11 +2,14 @@
 
 export default function ProfileCard() {
     return (
-        <>
+        <div>
             <span className="avatar-left">[</span>
             <img className="avatar" src="./default.gif" />
             <span className="avatar-right">]</span>
             <h1>[userData.name]'s Profile</h1>
-        </>
+            <p>
+                <i className="material-icons-round" title="email verified">verified</i> (or <i className="material-icons-round" title="email not verified">cancel</i>)
+            </p>
+        </div>
     );
 }
