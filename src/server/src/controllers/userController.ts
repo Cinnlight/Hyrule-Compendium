3b2dd2dc0d2717a7e2a1e08d99e28b2cc37a4447
MagicInUse <jacob.norr@live.com>
2025-02-03 22:08:56 -0700

    user api routing and req/res

--- src/server/src/controllers/userController.ts
+++ src/server/src/controllers/userController.ts
@@ -6,26 +6,70 @@ class UserController {
     getAllUsers = async (req: Request, res: Response) => {
         try {
             const users = await Users.findAll({
-                include: [
-                    {
-                        model: Users,
-                        attributes: [
-                            'id',
-                            'login',
-                            'display_name',
-                            'email',
-                            'auth_level',
-                            'email_val',
-                            'created_at',
-                            'updated_at',
-                            'avatar_url',
-                        ],
-                    },
+                attributes: [
+                    'id',
+                    'login',
+                    'display_name',
+                    'email',
+                    'auth_level',
+                    'email_val',
+                    'created_at',
+                    'updated_at',
+                    'avatar_url',
                 ],
             });
             res.json(users);
         } catch (error) {
-            res.status(500).json({ error: 'Failed to retrieve comments' });
+            console.error('Error:', error);
+            res.status(500).json({ error: 'Failed to retrieve users' });
+        }
+    };
+
+    // Get user by id
+    getUserById = async (req: Request, res: Response) => {
+        try {
+            const user = await Users.findOne({
+                where: { id: req.body.id },
+                attributes: [
+                    'id',
+                    'login',
+                    'display_name',
+                    'email',
+                    'auth_level',
+                    'email_val',
+                    'created_at',
+                    'updated_at',
+                    'avatar_url',
+                ],
+            });
+            res.json(user);
+        } catch (error) {
+            console.error('Error:', error);
+            res.status(500).json({ error: 'Failed to retrieve user' });
+        }
+    };
+
+    // Get user by login
+    getUserByLogin = async (req: Request, res: Response) => {
+        try {
+            const user = await Users.findOne({
+                where: { login: req.body.login },
+                attributes: [
+                    'id',
+                    'login',
+                    'display_name',
+                    'email',
+                    'auth_level',
+                    'email_val',
+                    'created_at',
+                    'updated_at',
+                    'avatar_url',
+                ],
+            });
+            res.json(user);
+        } catch (error) {
+            console.error('Error:', error);
+            res.status(500).json({ error: 'Failed to retrieve user' });
         }
     };
 };
--- src/server/src/routes/api/users.ts
+++ src/server/src/routes/api/users.ts
@@ -3,7 +3,13 @@ import UserController from '../../controllers/userController.js';
 
 const userRouter = express.Router();
 
-// Get all comments and their reaction(s) and reaction count(s)
+// Get all users and their everything
 userRouter.get('/all', UserController.getAllUsers);
+
+// Get user by id
+userRouter.get('/id', UserController.getUserById);
+
+// Get user by login
+userRouter.get('/login', UserController.getUserByLogin);
 
 export default userRouter;
