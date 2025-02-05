44ff7ec355f765ab24e87c68ef671d1600e6098c
MagicInUse <jacob.norr@live.com>
2025-02-04 20:30:50 -0700

    deployment successful

--- src/server/dist/src/routes/api/comments.js
+++ src/server/dist/src/routes/api/comments.js
@@ -4,7 +4,7 @@ const commentRouter = express.Router();
 // Get all comments and their reaction(s) and reaction count(s)
 commentRouter.get('/all', CommentController.getAllComments);
 // Get all comments for a specific page
-commentRouter.get('/page', CommentController.getCommentsByPageId);
+commentRouter.post('/page', CommentController.getCommentsByPageId);
 // Create a new comment
 commentRouter.post('/create', CommentController.createComment);
 // Update a comment
